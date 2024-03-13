8889903cf107e08966d8d91c527dce80b5b26716
Matias <matias.semelman@protonmail.com>
2024-03-13 10:34:09 -0300

    Update siteUrl in next-sitemap.js

--- next-sitemap.js
+++ next-sitemap.js
@@ -1,6 +1,6 @@
 /* eslint-env node */
 module.exports = {
-  siteUrl: 'reactresume.com',
+  siteUrl: 'https://semelman-portfolio.vercel.app/',
   exclude: ['/404*', '/500*'],
   transform: async (config, path) => {
     return {
