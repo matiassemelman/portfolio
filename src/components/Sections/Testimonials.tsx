0df87b41af6dd3575dcddecd5a9d40cc765ec73d
Matias <matias.semelman@protonmail.com>
2024-03-13 10:10:15 -0300

    Add next/image for image rendering in Testimonials component

--- src/components/Sections/Testimonials.tsx
+++ src/components/Sections/Testimonials.tsx
@@ -1,4 +1,5 @@
 import classNames from 'classnames';
+import Image from 'next/image';
 import {FC, memo, UIEventHandler, useCallback, useEffect, useMemo, useRef, useState} from 'react';
 
 import {isApple, isMobile} from '../../config';
@@ -123,7 +124,7 @@ const Testimonial: FC<{testimonial: Testimonial; isActive: boolean}> = memo(
       {image ? (
         <div className="relative h-14 w-14 shrink-0 sm:h-16 sm:w-16">
           <QuoteIcon className="absolute -left-2 -top-2 h-4 w-4 stroke-black text-white" />
-          <img className="h-full w-full rounded-full" src={image} />
+          <Image alt="Photo" className="h-full w-full rounded-full" src={image} />
         </div>
       ) : (
         <QuoteIcon className="h-5 w-5 shrink-0 text-white sm:h-8 sm:w-8" />
